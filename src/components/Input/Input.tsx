5a409c838821608a659b04e8551e9503131e62e0
dmitriyrotaenko <rotaenko.dmitriy@gmail.com>
2025-11-16 22:36:04 +0300

    feat: add Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import './Input.scss';
+
+type InputProps = {
+	className?: string;
+	type: string;
+	value: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	placeholder?: string;
+};
+
+const Input = (props: InputProps) => {
+	const { type, value, onChange, placeholder, className } = props;
+
+	return (
+		<input
+			className={`input ${className || ''}`}
+			type={type}
+			value={value}
+			onChange={onChange}
+			placeholder={placeholder}
+		/>
+	);
+};
+
+export default Input;
